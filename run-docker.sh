4cbad0ed520f45b78640042d49a5d87ffe988d6e
markoniemi <marko.j.niemi@gmail.com>
2020-07-17 13:20:53 +0300

    Enable different versions of services.

--- run-docker.sh
+++ run-docker.sh
@@ -1,9 +1,15 @@
 PORT=${1:-8082}
+PATH_PREFIX=${2:-demo/v1}
+VERSION=${3:-v1}
 docker run \
 -it --rm \
 -d \
 --name demo-application-${PORT} \
 -e PORT=${PORT} \
+-e CONSUL_HOST=consul \
+-e CONSUL_PORT=8500 \
+-e PATH_PREFIX=${PATH_PREFIX} \
+-e VERSION=${VERSION} \
 -p ${PORT}:${PORT} \
 --network traefik-consul-demo_microservices \
 --network-alias demo \
