b0af7b1d13cc563cf1c7b3e6d3cbcee81964ec1f
markoniemi <marko.j.niemi@gmail.com>
2020-07-19 15:57:21 +0300

    Update portainer, consul and traefik. Support different versions of services.

--- src/main/java/org/traefik/demo/DemoApplication.java
+++ src/main/java/org/traefik/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +14,23 @@ import lombok.extern.log4j.Log4j2;
 @EnableDiscoveryClient
 @RestController
 @Log4j2
+@RequestMapping("/demo")
 public class DemoApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(DemoApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 
-    @RequestMapping("/health")
-    public String health() {
-        return "OK";
+    @RequestMapping("/v1/hello")
+    public String home() {
+        log.info("/v1/hello");
+        return "Hello world";
     }
 
-    @RequestMapping("/hello")
-    public String home() {
-        log.info("/hello");
-        return "Hello world";
+    @RequestMapping("/v2/hello/{name}")
+    public String home(@PathVariable("name") String name) {
+        log.info("/v2/hello/" + name);
+        return "Hello world: " + name;
     }
 
 }
