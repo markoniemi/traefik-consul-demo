4ca64cbd01188f9f7da450efe7f1308cfe3232a2
Marko Niemi <marko.j.niemi@gmail.com>
2020-08-28 21:03:03 +0300

    dockerfile-maven-plugin -> 1.4.13. Dockerfile improvements.

--- user-application/src/main/java/example/user/controller/FooterController.java
+++ user-application/src/main/java/example/user/controller/FooterController.java
@@ -1,13 +1,15 @@
 package example.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
+@RefreshScope
 public class FooterController {
-    @Value("runtime.environment")
+    @Value("${runtime.environment:dev}")
     private String environment;
 
     @ModelAttribute
